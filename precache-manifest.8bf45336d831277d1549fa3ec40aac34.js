5b34293ee7c4d9995e265e528f759f69c20ee92b
tonynguyen111997 <tonynguyen111997@gmail.com>
2018-11-27 06:01:53 +0000

    Updates

---
+++ precache-manifest.8bf45336d831277d1549fa3ec40aac34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b9f57e2e0bf40b655e9",
+    "url": "/react-project-manager-app/static/js/runtime~main.4b9f57e2.js"
+  },
+  {
+    "revision": "19c9d1e0635accab4c8e",
+    "url": "/react-project-manager-app/static/js/main.19c9d1e0.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/react-project-manager-app/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "19c9d1e0635accab4c8e",
+    "url": "/react-project-manager-app/static/css/main.f04b8fe7.chunk.css"
+  },
+  {
+    "revision": "edf06e0dbaf9de52c2640e3a2393394f",
+    "url": "/react-project-manager-app/index.html"
+  }
+];
